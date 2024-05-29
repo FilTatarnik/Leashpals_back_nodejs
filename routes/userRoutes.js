5c2eb84efdff62affd1af1d591032a1c4c2ae893
Filip Tatarnik <ftatarnik@gmail.com>
2024-05-28 20:05:03 -0500

    Started with login with jwt tokens and bcrypt. Saving for nowcontinue tomorrow

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { getAllUsers } = require('../controllers/UserController');
 const router = express.Router();
 const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
 
 // Route for User Registration
 router.post('/register', async (req, res) => {
@@ -32,9 +35,30 @@ router.post('/login', async (req, res) => {
     //getting the username and password from the request body
     const { username, password } = req.body;
 
+    try {
+        //find user by username
+        const user = await User.findOne({ where: { username }});
+        //If User doesn't exist, send error
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid username or password' });
+        }
+        //check password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        //if password doesn't match, send error
+        if(!isMatch) {
+            return res.status(400).json({ error: 'Invalid username or password' });
+        }
+        //if login is successful, generate and send token
+        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
     //Todo add validation and err handling
 
-    //check password
+
 
     //log user in by setting a cookie or session variable
     
