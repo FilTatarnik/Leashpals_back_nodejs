efbf517c189eb281300cec30e3b090ce2debece1
Filip Tatarnik <ftatarnik@gmail.com>
2025-01-14 11:30:21 -0600

    POST create single appointment is done

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AppointmentController = require('../controllers/DogController');
 const { getAllAppointments, getAppointment } = require('../controllers/AppointmentController');
+const Appointment = require('../models/AppointmentModel');
 const router = express.Router();
 
 //GET all Appointments
@@ -9,4 +10,19 @@ router.get('/', getAllAppointments);
 //GET Appointment by ID
 router.get('/:id', getAppointment);
 
+//POST create Appointment
+router.post('/register', async (req, res) => {
+    const { dog_id, walker_id } = req.body;
+    try {
+        const newAppointment = await Appointment.create({ dog_id, walker_id });
+            if (!newAppointment) {
+                console.log('Appointment Creation Failed');
+                return res.status(500).json({ error: 'Appointment creation failed' });
+            }
+            res.json(newAppointment);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Appointment creation failed.'});
+    }
+});
 module.exports = router;
